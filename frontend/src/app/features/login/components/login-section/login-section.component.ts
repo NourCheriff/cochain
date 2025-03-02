f464f2b83126b0ace05ca52bb1e2c6bcfb6434a1
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-02 23:38:42 +0100

    add: login page

---
+++ frontend/src/app/features/login/components/login-section/login-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoginFormComponent } from '../form/form.component';
+
+@Component({
+  selector: 'app-login-section',
+  imports: [LoginFormComponent],
+  templateUrl: './login-section.component.html',
+  styleUrl: './login-section.component.css'
+})
+export class LoginSectionComponent {
+
+}
