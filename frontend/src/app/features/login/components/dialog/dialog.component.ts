dc8c562a614e61aad1da4b543d90ab7b4bfb286c
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-03 13:30:14 +0100

    update: dialog logic and style
    update: css style for dialog component
    add: disabling send button when otp is not set or the login request was done
    add: event emitter to resend the OTP from the parent component to avoid rewriting the requestOtp function
    add: snackbars to give feedback to the user

--- frontend/src/app/features/login/components/dialog/dialog.component.ts
+++ frontend/src/app/features/login/components/dialog/dialog.component.ts
@@ -1,16 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import {
   MAT_DIALOG_DATA,
-  MatDialog,
   MatDialogActions,
-  MatDialogClose,
   MatDialogContent,
   MatDialogRef,
   MatDialogTitle,
 } from '@angular/material/dialog';
-import { FormControl } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatSnackBar, MatSnackBarModule} from '@angular/material/snack-bar';
+
+interface DialogData {
+  email: string;
+}
 
 @Component({
   selector: 'app-login-dialog',
@@ -20,12 +23,33 @@ import {MatButtonModule} from '@angular/material/button';
     MatDialogTitle,
     MatDialogContent,
     MatDialogActions,
-    MatDialogClose,
+    ReactiveFormsModule,
+    MatSnackBarModule,
   ],
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.css'
 })
 export class LoginDialogComponent {
+  @Output() resendOtpEvent = new EventEmitter();
+  isLoginRequestSent = false;
+
+  private _snackBar = inject(MatSnackBar);
   readonly dialogRef = inject(MatDialogRef<LoginDialogComponent>);
-  otp = new FormControl("");
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA)
+  otp = new FormControl("", Validators.required);
+
+  resendOtp() {
+    this._showSnackbar('OTP resent');
+    this.resendOtpEvent.emit();
+  }
+
+  login() {
+    this.isLoginRequestSent = true; // disable send button
+    this._showSnackbar('Login request sent successfully');
+    console.log(`OTP Sent: ${this.data.email}, ${this.otp.value}`); /** inject and call auth service or do it in the form component */
+  }
+
+  private _showSnackbar(message: string) {
+    this._snackBar.open(message, undefined, { duration: 3000 });
+  }
 }
