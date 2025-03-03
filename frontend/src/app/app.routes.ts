ee36f6054d206ef19c388b8f488cbe83ed2fb13d
Your Name <you@example.com>
2025-03-03 23:54:06 +0100

    Navbar component and certificate page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/pages/login/login.component';
 import { ContractsComponent } from './features/contracts/pages/contracts/contracts.component';
+import { CertificatesComponent } from './features/certificates/pages/certificates/certificates.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   {
     path: 'login',
     title: 'Login',
@@ -10,7 +16,12 @@ export const routes: Routes = [
   },
   {
     path: 'contracts',
-    title: 'Contratti',
+    title: 'Contracts',
     component: ContractsComponent,
+  },
+  {
+    path: 'certificates',
+    title: 'Certificates',
+    component: CertificatesComponent,
   }
 ];
--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatIconModule,MatDividerModule,MatButtonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
