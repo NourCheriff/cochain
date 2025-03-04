8257cf66d6869ecd96e190c30a7666ab3e6da3bc
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-04 11:19:16 +0100

    fix: navbar style and add routes to navbar

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 @Component({
   selector: 'app-navbar',
-  imports: [MatIconModule,MatDividerModule,MatButtonModule],
+  imports: [MatIconModule, MatDividerModule, MatButtonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
