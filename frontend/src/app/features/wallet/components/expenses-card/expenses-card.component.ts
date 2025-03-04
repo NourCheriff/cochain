0f35d62d491989cf53d3d6ceff7fe70de9365e00
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-04 22:53:18 +0100

    add: wallet info, incomes and expenses cards components

---
+++ frontend/src/app/features/wallet/components/expenses-card/expenses-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon'
+
+@Component({
+  selector: 'app-expenses-card',
+  imports: [MatIconModule],
+  templateUrl: './expenses-card.component.html',
+  styleUrl: './expenses-card.component.css'
+})
+export class ExpensesCardComponent {
+
+  @Input() expenses: number = 0;
+
+}
---
+++ frontend/src/app/features/wallet/components/wallet-info-card/wallet-info-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+
+@Component({
+  selector: 'app-wallet-info-card',
+  imports: [MatButtonModule, MatIconModule],
+  templateUrl: './wallet-info-card.component.html',
+  styleUrl: './wallet-info-card.component.css'
+})
+export class WalletInfoCardComponent {
+  @Input() walletID: string = '0x1234';
+  @Input() balance: number = 0;
+}
