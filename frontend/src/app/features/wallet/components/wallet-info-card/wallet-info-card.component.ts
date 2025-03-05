c37ab98f227521c1d28bfd5e6653c57f88734623
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-05 18:16:14 +0100

    fix: transactions table style

--- frontend/src/app/features/wallet/components/wallet-info-card/wallet-info-card.component.ts
+++ frontend/src/app/features/wallet/components/wallet-info-card/wallet-info-card.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon'
 
 @Component({
   selector: 'app-wallet-info-card',
