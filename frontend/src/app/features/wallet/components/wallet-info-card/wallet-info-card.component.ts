a6e6610ccf0d4173e4e564bde28e68b099efa8fd
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-06 14:23:06 +0100

    add: new transaction dialog

--- frontend/src/app/features/wallet/components/wallet-info-card/wallet-info-card.component.ts
+++ frontend/src/app/features/wallet/components/wallet-info-card/wallet-info-card.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon'
+import { TransactionsDialogComponent } from '../transactions-dialog/transactions-dialog.component';
 
 @Component({
   selector: 'app-wallet-info-card',
@@ -11,4 +13,10 @@ import { MatIconModule } from '@angular/material/icon'
 export class WalletInfoCardComponent {
   @Input() walletID: string = '0x1234';
   @Input() balance: number = 0;
+
+  readonly dialog = inject(MatDialog);
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TransactionsDialogComponent);
+  }
 }
