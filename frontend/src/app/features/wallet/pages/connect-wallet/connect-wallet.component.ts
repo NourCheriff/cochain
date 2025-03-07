c71cc1b92f7917691d327f0fd1f3e28129f1e243
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-07 22:29:29 +0100

    feat: show spinner after clicking the connection button

--- frontend/src/app/features/wallet/pages/connect-wallet/connect-wallet.component.ts
+++ frontend/src/app/features/wallet/pages/connect-wallet/connect-wallet.component.ts
@@ -1,17 +1,19 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-connect-wallet',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatProgressSpinnerModule],
   templateUrl: './connect-wallet.component.html',
   styleUrl: './connect-wallet.component.css'
 })
 export class ConnectWalletComponent {
   @Output() connectWalletEvent = new EventEmitter();
+  isLoading: boolean = false; // flag to display the connect button or the spinner
 
   connectWallet(): void {
     this.connectWalletEvent.emit();
+    this.isLoading = true;
   }
-
 }
