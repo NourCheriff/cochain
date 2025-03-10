67b0565a2dedeb2947aa64cb505f88ac22b1d45b
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-10 21:54:38 +0100

    add: environment config files

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://cochainapi-app-20250308155408.bravebeach-6563b686.northeurope.azurecontainerapps.io',
+  apiEndpoint: '/api',
+};
