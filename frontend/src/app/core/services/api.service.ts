77c95667a7fe107daad44737f0e61f746fe6d9af
Your Name <you@example.com>
2025-03-10 23:05:59 +0100

    created api service

---
+++ frontend/src/app/core/services/api.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+//import { AuthService } from './auth.service';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseHttpService<T> {
+
+  private readonly API_BASE_URL = environment.baseUrl;
+  private readonly header = new HttpHeaders({
+      'Content-Type': 'application/json'
+  });
+
+  constructor(protected http: HttpClient, /*protected authService: AuthService*/) {}
+
+  private createParams(params?: { [key: string]: any }): HttpParams {
+    let httpParams = new HttpParams();
+    if (params) {
+      Object.keys(params).forEach((key) => {
+        if (params[key] !== undefined && params[key] !== null) {
+          httpParams = httpParams.set(key, params[key]);
+        }
+      });
+    }
+    return httpParams;
+  }
+
+  getAll<T>(endpoint: string, params?: { [key: string]: any } ) : Observable<T[]>{
+    return this.http.get<T[]>(`${this.API_BASE_URL}/${endpoint}`,{
+      headers: this.header,
+      params: this.createParams(params)
+    });
+  }
+
+  getById<T>(endpoint: string, id: string) : Observable<T>{
+    return this.http.get<T>(`${this.API_BASE_URL}/${endpoint}/${id}`,{
+      headers: this.header,
+    });
+  }
+
+  add<T>(endpoint: string, data: T): Observable<T>{
+     return this.http.post<T>(`${this.API_BASE_URL}/${endpoint}`, data, {
+      headers: this.header
+    });
+  }
+
+  update<T>(endpoint: string, newData: T, id: string): Observable<T>{
+    return this.http.post<T>(`${this.API_BASE_URL}/${endpoint}/${id}`, newData, {
+      headers: this.header
+    });
+  }
+
+  delete<T>(endpoint: string, id: string): Observable<T>{
+    return this.http.post<T>(`${this.API_BASE_URL}/${endpoint}/${id}`,{
+      headers: this.header
+    });
+  }
+
+}
