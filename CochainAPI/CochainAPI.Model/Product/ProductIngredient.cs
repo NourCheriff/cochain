6c2455790a7322f2166d8a3bcf69816c6afd4ef4
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-11 17:52:14 +0100

    fix: prevent ProductInfo property of ProductIngredient to be serialized to Json

--- CochainAPI/CochainAPI.Model/Product/ProductIngredient.cs
+++ CochainAPI/CochainAPI.Model/Product/ProductIngredient.cs
@@ -1,10 +1,13 @@
-﻿namespace CochainAPI.Model.Product
+﻿using System.Text.Json.Serialization;
+
+namespace CochainAPI.Model.Product
 {
     public class ProductIngredient
     {
         public Guid IngredientId { get; set; }
         public ProductInfo? Ingredient { get; set; }
         public Guid ProductInfoId { get; set; }
+        [JsonIgnore]
         public ProductInfo? ProductInfo { get; set; }
     }
 }
