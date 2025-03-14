fd5dea084e2907723cde8746ef5e10a76f5b57bd
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-14 23:09:48 +0100

    fix: restrict logged in users from visiting login route

--- frontend/src/app/features/login/pages/login/login.component.ts
+++ frontend/src/app/features/login/pages/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { WelcomeComponent } from "../../components/welcome/welcome.component";
 import { LoginSectionComponent } from "../../components/login-section/login-section.component";
+import { AuthService } from 'src/app/core/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,6 +10,13 @@ import { LoginSectionComponent } from "../../components/login-section/login-sect
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  ngOnInit(): void {
+    if (this.authService.token)
+      this.router.navigateByUrl('');
+  }
 
 }
