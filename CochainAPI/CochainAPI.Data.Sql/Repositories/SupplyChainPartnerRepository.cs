c9ed1722fbbbb65d983abffcd7a2423b9a1cab37
Matteo04x <59709050+Matteo04x@users.noreply.github.com>
2025-03-14 18:58:03 +0100

    added SCP type when getting all supply chain partners

--- CochainAPI/CochainAPI.Data.Sql/Repositories/SupplyChainPartnerRepository.cs
+++ CochainAPI/CochainAPI.Data.Sql/Repositories/SupplyChainPartnerRepository.cs
@@ -30,6 +30,8 @@ namespace CochainAPI.Data.Sql.Repositories
                 .Take(pageSize.Value);
             }
 
+            query = query.Include(x => x.SupplyChainPartnerType);
+
             return await query.ToListAsync();
         }
     }
