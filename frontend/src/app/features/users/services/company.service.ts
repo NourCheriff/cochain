4b3504b8016a372773e815c3b0950e303027f2b2
Matteo04x <59709050+Matteo04x@users.noreply.github.com>
2025-03-14 21:16:01 +0100

    added company service to add an scp pratner

--- frontend/src/app/features/users/services/company.service.ts
+++ frontend/src/app/features/users/services/company.service.ts
@@ -1,6 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BaseHttpService } from 'src/app/core/services/api.service';
 import { CertificationAuthority } from 'src/models/company-entities/certification-authority.model';
 import { SupplyChainPartner } from 'src/models/company-entities/supply-chain-partner.model';
@@ -18,5 +19,9 @@ export class CompanyService {
 
   getAllSupplyChainPartners(): Observable<SupplyChainPartner[]>{
     return this.apiService.getAll('api/SupplyChainPartner')
+  }
+
+  addSupplyChainPartner(newSCP: SupplyChainPartner){
+    return this.apiService.add('api/SupplyChainPartner/addSCP', {})
   }
 }
