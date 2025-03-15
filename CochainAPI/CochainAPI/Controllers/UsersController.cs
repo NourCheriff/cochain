15e75b1e2fcfeef4d028134273be291593d1cb3b
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-15 18:24:52 +0100

    fix: split user insertion and update endpoint to two endpoints
    this update is necessary to avoid the Id validation error when inserting user

--- CochainAPI/CochainAPI/Controllers/UsersController.cs
+++ CochainAPI/CochainAPI/Controllers/UsersController.cs
@@ -55,11 +55,23 @@ namespace CochainAPI.Controllers
             return Ok(response);
         }
 
+        [HttpPost("AddUser")]
+        [Authorize]
+        public async Task<IActionResult> AddUser([FromBody] User userObj)
+        {
+            var response = await _userService.AddUser(userObj);
+            if (response == null)
+            {
+                return BadRequest(new { message = "Insertion of user failed" });
+            }
+            return Ok(response);
+        }
+
         [HttpPost("UpdateUser")]
         [Authorize]
-        public async Task<IActionResult> Post([FromBody] User userObj)
+        public async Task<IActionResult> UpdateUser([FromBody] User userObj)
         {
-            var response = await _userService.AddAndUpdateUser(userObj);
+            var response = await _userService.UpdateUser(userObj);
             if (response == null)
             {
                 return BadRequest(new { message = "User not found" });
