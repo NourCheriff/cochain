62aaaa9ef4a31024dd333b32b24cfde9597fa34d
Your Name <you@example.com>
2025-03-16 11:15:53 +0100

    removed commented model

--- frontend/src/models/documents/base-document.model.ts
+++ frontend/src/models/documents/base-document.model.ts
@@ -1,15 +1,4 @@
-import { User } from "../auth/user.model";
 import { Base } from "../base.model";
-import { SupplyChainPartner } from "../company-entities/supply-chain-partner.model";
-
-/*
-export interface BaseDocument extends Base {
-  path?: string;
-  type?: string;
-  userEmitter: User;
-  supplyChainPartnerReceiver: SupplyChainPartner;
-}
-*/
 
 export interface BaseDocument extends Base {
   path?: string;
