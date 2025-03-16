92e296b77978a42c0f2c350fddf944258c225926
Paolo Roselli <paolo.roselli02@gmail.com>
2025-03-16 15:52:10 +0100

    restore bad request response on user controller

--- CochainAPI/CochainAPI/Controllers/UsersController.cs
+++ CochainAPI/CochainAPI/Controllers/UsersController.cs
@@ -47,14 +47,10 @@ namespace CochainAPI.Controllers
         public async Task<IActionResult> RequestPassword(AuthenticateRequest model)
         {
             var response = await _authService.GenerateTemporaryCredentials(model);
-            /*
             if (!response)
                 return BadRequest(new { message = "Username is incorrect" });
             
             return Ok(response);
-            */
-            
-            return Ok();
         }
 
         [HttpPost("Login")]
