f181ea177c2e358bce67131a64949cda99be23f5
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-21 16:52:14 +0100

    update: make URL, Cookies and QueryString as optional into Log

---
+++ CochainAPI/CochainAPI.Data.Sql/Migrations/20250321155134_UpdateLogs.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CochainAPI.Data.Sql.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateLogs : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_User_PartnerOrAuthority",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "URL",
+                table: "Log",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QueryString",
+                table: "Log",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cookies",
+                table: "Log",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "0a317b04-2f87-4b08-8ad8-597103527584",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "2fe7b805-afed-46d2-8fb8-80c550c25f14", "843259e9-3799-4195-8bea-95505c35f6fa" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "3542da56-0de3-4797-a059-effff257f63d",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "8526d566-e20b-4ed4-a598-c5942a53bb50", "1637b5f6-8ec4-4f58-aa1b-5d09d07e2e87" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "5e4b0ca8-aa85-417a-af23-035ac1b555cd",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "90233221-c836-4d8c-b6fc-846c496c60e7", "f6af9ca5-017a-4e1b-adcd-c8ddd91cd620" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "a12c3708-0486-4603-b1a5-46d252e79082",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "26bba018-90b5-493c-9614-8d9f967adabc", "35586e2a-be31-4aba-9a3a-b8fd6697d063" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "f4242b5f-4b39-45fc-802e-391293414546",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "b0eb5196-627e-40cb-ad9c-ad088933c1f7", "bf7212c0-81ae-4e26-804c-359a47aa7bcc" });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "AccessFailedCount", "CertificationAuthorityId", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "IsActive", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "Phone", "PhoneNumber", "PhoneNumberConfirmed", "Role", "SecurityStamp", "SupplyChainPartnerId", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "ad00648b-a031-432d-b007-6a0829cf5292", 0, null, "0d070cf5-6eea-4c24-b439-4a158cee3a99", null, false, "System", true, "System", false, null, null, null, null, null, null, false, null, "09adff98-3a90-43e6-ad2c-62d689cdacbd", null, false, "admin@cochain.com" });
+
+            migrationBuilder.InsertData(
+                table: "UserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "8e342ad6-78d9-4aee-abe5-245b1fae6c4a", "ad00648b-a031-432d-b007-6a0829cf5292" });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_User_PartnerOrAuthority",
+                table: "User",
+                sql: "(\"SupplyChainPartnerId\" IS NOT NULL AND \"CertificationAuthorityId\" IS NULL)\n                    OR (\"SupplyChainPartnerId\" IS NULL AND \"CertificationAuthorityId\" IS NOT NULL)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_User_PartnerOrAuthority",
+                table: "User");
+
+            migrationBuilder.DeleteData(
+                table: "UserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "8e342ad6-78d9-4aee-abe5-245b1fae6c4a", "ad00648b-a031-432d-b007-6a0829cf5292" });
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "ad00648b-a031-432d-b007-6a0829cf5292");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "URL",
+                table: "Log",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QueryString",
+                table: "Log",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Cookies",
+                table: "Log",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "0a317b04-2f87-4b08-8ad8-597103527584",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "14538df9-eff1-49b0-87bf-c8b097167ba1", "a7ba693b-93a8-47f4-b3a7-7eb18cb181d5" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "3542da56-0de3-4797-a059-effff257f63d",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "f97094ca-a26e-49b6-8bf8-949a8c13e912", "73861d6c-6a19-46c5-92b4-a6d1527b1aff" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "5e4b0ca8-aa85-417a-af23-035ac1b555cd",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "29c57c37-62df-45ef-9c17-6f2dee08e6eb", "fa9baacb-fad0-400c-bb6d-039fd74f4f27" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "a12c3708-0486-4603-b1a5-46d252e79082",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "d9c313f2-39dd-4986-b54a-370db69b4279", "a496e98f-b6f1-4961-8c41-0ea8d20267da" });
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: "f4242b5f-4b39-45fc-802e-391293414546",
+                columns: new[] { "ConcurrencyStamp", "SecurityStamp" },
+                values: new object[] { "d58ebf1b-2fae-4bff-ae54-fa42aeb55d19", "6c098cdb-2122-49c2-ab93-169cfe3cb0ec" });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_User_PartnerOrAuthority",
+                table: "User",
+                sql: "(\"SupplyChainPartnerId\" IS NOT NULL AND \"CertificationAuthorityId\" IS NULL)\r\n                    OR (\"SupplyChainPartnerId\" IS NULL AND \"CertificationAuthorityId\" IS NOT NULL)");
+        }
+    }
+}
