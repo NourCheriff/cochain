ff33d2ab295b2b7a25c950ff6e634cd3f2380b1e
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-21 22:18:49 +0100

    add: severity and roles enums to frontend

--- frontend/src/types/roles.enum.ts
+++ frontend/src/types/roles.enum.ts
@@ -1,5 +1,12 @@
 export enum Role {
   SysAdmin = 'SystemAdmin',
+  AdminSCP = 'AdminSCP',
+  AdminCA = 'AdminCA',
+  UserSCP = 'UserSCP',
+  UserCA = 'UserCA',
+  SCPTransporter = 'SCPTransporter',
+  SCPRawMaterial = 'SCPRawMaterial',
+  SCPTransformator = 'SCPTransformator',
   User = 'User',
   Admin = 'Admin',
 }
