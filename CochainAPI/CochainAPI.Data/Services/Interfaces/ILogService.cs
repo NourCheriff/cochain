daec30f3c9ca0fb3e65cf506b5f9cbe1c74f7494
Paolo Roselli <paolo.roselli02@gmail.com>
2025-03-21 17:53:33 +0100

    get logs endpoint

--- CochainAPI/CochainAPI.Data/Services/Interfaces/ILogService.cs
+++ CochainAPI/CochainAPI.Data/Services/Interfaces/ILogService.cs
@@ -1,11 +1,10 @@
 ﻿using CochainAPI.Model.Helper;
 using CochainAPI.Model.Utils;
 
-namespace CochainAPI.Data.Sql.Repositories.Interfaces
+namespace CochainAPI.Data.Services.Interfaces
 {
-    public interface ILogRepository
+    public interface ILogService
     {
         Task<Page<Log>> GetLogs(string? severity, string? queryParam, int? pageNumber, int? pageSize);
-        Task<bool> AddLog(Log log);
     }
 }
---
+++ CochainAPI/CochainAPI.Data/Services/LogService.cs
@@ -0,0 +1,20 @@
+﻿using CochainAPI.Data.Services.Interfaces;
+using CochainAPI.Data.Sql.Repositories.Interfaces;
+using CochainAPI.Model.Helper;
+using CochainAPI.Model.Utils;
+
+namespace CochainAPI.Data.Services
+{
+    public class LogService : ILogService
+    {
+        private readonly ILogRepository _logRepository;
+        public LogService(ILogRepository logRepository) 
+        {
+            _logRepository = logRepository;
+        }
+        public async Task<Page<Log>> GetLogs(string? severity, string? queryParam, int? pageNumber, int? pageSize)
+        {
+            return await _logRepository.GetLogs(severity, queryParam, pageNumber, pageSize);
+        }
+    }
+}
