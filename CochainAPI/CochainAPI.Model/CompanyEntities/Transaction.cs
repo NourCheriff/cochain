f8868adebc3f35c920ac38d902ac2d8703ddca58
Matteo04x <59709050+Matteo04x@users.noreply.github.com>
2025-03-21 12:47:52 +0100

    renamed EmissionTransaction to Transaction, moved it in CompanyEntities, and added SCP emitter and receiver

--- CochainAPI/CochainAPI.Model/CompanyEntities/Transaction.cs
+++ CochainAPI/CochainAPI.Model/CompanyEntities/Transaction.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace CochainAPI.Model.Transaction
+namespace CochainAPI.Model.CompanyEntities
 {
     [PrimaryKey(nameof(TransactionHash))]
-    public class EmissionTransaction
+    public class Transaction
     {
         public string TransactionHash { get; set; }
         public string WalletIdEmitter { get; set; }
+        public SupplyChainPartner? supplyChainPartnerEmitter { get; set; }
         public string WalletIdReceiver { get; set; }
+        public SupplyChainPartner? supplyChainPartnerReceiver { get; set; }
     }
 }
