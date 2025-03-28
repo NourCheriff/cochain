8889f20d4da892efd1d1003da504cd391efcd47e
Matteo04x <59709050+Matteo04x@users.noreply.github.com>
2025-03-28 01:53:32 +0100

    fix: endpoint logic GetProductsByIds in GetIngredientsByProductInfoId

--- CochainAPI/CochainAPI.Data/Services/Interfaces/IProductService.cs
+++ CochainAPI/CochainAPI.Data/Services/Interfaces/IProductService.cs
@@ -9,7 +9,7 @@ namespace CochainAPI.Data.Services.Interfaces
         Task<List<ProductCategory>> GetCategories();
         Task<List<Product>?> GetGenericProducts(Guid id);
         Task<ProductInfo?> GetProductById(Guid id);
-        Task<List<ProductInfo>?> GetProductsByIds(Guid[] id);
+        Task<List<ProductInfo>?> GetIngredientsByProductInfoId(Guid id);
         Task<ProductInfo?> UpdateProduct(ProductInfo productObj);
         Task<Page<ProductInfo>?> GetProductsOfSCP(Guid id, string? queryParam, int? pageNumber, int? pageSize);
         Task<Page<ProductInfo>> GetProducts(string? productName, string? scpName, int? pageNumber, int? pageSize);
