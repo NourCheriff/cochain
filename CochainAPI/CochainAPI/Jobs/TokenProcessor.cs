331923eb2cda6d69d93f510eab0e7af926a2e06f
shalym <sacchettisamuele@gmail.com>
2025-03-28 11:08:47 +0100

    fix: tokenProcessor to execute transaction with a main account

--- CochainAPI/CochainAPI/Jobs/TokenProcessor.cs
+++ CochainAPI/CochainAPI/Jobs/TokenProcessor.cs
@@ -2,69 +2,101 @@ using CochainAPI.Data.Services.Interfaces;
 using Quartz;
 using Nethereum.Web3;
 using Nethereum.Web3.Accounts;
+using CochainAPI.Data.Sql.Repositories.Interfaces;
 
 namespace CochainAPI.Jobs
 {
     public class TokenProcessor : IJob
     {
+        private readonly ILogRepository _logRepository;
         private readonly IProductLifeCycleService _lifeCycleService;
         private readonly ISupplyChainPartnerService _supplyChainPartnerService;
         private readonly ICarbonOffsettingActionService _actionService;
         private readonly IConfiguration _config;
         private readonly string _blockchainURL;
 
-        public TokenProcessor(IProductLifeCycleService lifeCycleService, ISupplyChainPartnerService supplyChainPartnerService, ICarbonOffsettingActionService actionService, IConfiguration config)
+        public TokenProcessor(
+            IProductLifeCycleService lifeCycleService,
+            ISupplyChainPartnerService supplyChainPartnerService,
+            ICarbonOffsettingActionService actionService,
+            IConfiguration config,
+            ILogRepository logRepository)
         {
             _config = config;
             _lifeCycleService = lifeCycleService;
             _supplyChainPartnerService = supplyChainPartnerService;
             _actionService = actionService;
+            _logRepository = logRepository;
             _blockchainURL = _config.GetValue<string>("BlockchainSettings:localhostURL")!;
         }
 
         public async Task Execute(IJobExecutionContext context)
         {
-            var productLifeCycles = await _lifeCycleService.GetProductLifeCyclesToBeProcessed();
-
-            foreach (var item in productLifeCycles)
+            try
             {
-                var walletId = item.SupplyChainPartner.WalletId;
-                var account = new Account(walletId);
-                var web3 = new Web3(account, _blockchainURL);
-                var credits = item.SupplyChainPartner.SupplyChainPartnerType!.Baseline - item.Emissions;
-                var transaction = await web3.Eth.GetEtherTransferService()
-                .TransferEtherAndWaitForReceiptAsync(walletId, (decimal)credits);
+                var mainAccount = new Account(_config.GetValue<string>("BlockchainSettings:mainAccountKey")!);
+                var web3 = new Web3(mainAccount, _blockchainURL);
 
-                var transactionId = transaction.TransactionHash;
-                item.EmissionTransactionId = transactionId;
-
-                var updateResult = await _supplyChainPartnerService.UpdateScpCredits(item.SupplyChainPartnerId, credits);
-
-                if (!updateResult)
+                var productLifeCycles = await _lifeCycleService.GetProductLifeCyclesToBeProcessed();
+                foreach (var item in productLifeCycles)
                 {
-                    item.IsEmissionProcessed = true;
-                    await _lifeCycleService.SaveProductLife(item);
+                    try
+                    {
+                        var walletId = item.SupplyChainPartner!.WalletId;
+                        var credits = item.SupplyChainPartner.SupplyChainPartnerType!.Baseline - item.Emissions;
+                        var transaction = await web3.Eth.GetEtherTransferService()
+                                                        .TransferEtherAndWaitForReceiptAsync(
+                                                            walletId,
+                                                            (decimal)credits,
+                                                            gasPriceGwei: 1
+                                                        );
+                        var transactionId = transaction.TransactionHash;
+                        item.EmissionTransactionId = transactionId;
+
+                        var updateResult = await _supplyChainPartnerService.UpdateScpCredits(item.SupplyChainPartnerId, credits);
+                        if (updateResult)
+                        {
+                            item.IsEmissionProcessed = true;
+                            await _lifeCycleService.SaveProductLife(item);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error while executing Job: " + ex);
+                    }
+                }
+
+                var offsettingActions = await _actionService.GetOffsettingActionsToBeProcessed();
+                foreach (var item in offsettingActions)
+                {
+                    try
+                    {
+                        var walletId = item.SupplyChainPartner!.WalletId;
+                        var transaction = await web3.Eth.GetEtherTransferService()
+                                                        .TransferEtherAndWaitForReceiptAsync(
+                                                            walletId,
+                                                            (decimal)item.Offset,
+                                                            gasPriceGwei: 1
+                                                        );
+                        var transactionId = transaction.TransactionHash;
+                        item.EmissionTransactionId = transactionId;
+
+                        var updateResult = await _supplyChainPartnerService.UpdateScpCredits(item.SupplyChainPartnerId, item.Offset);
+                        if (updateResult)
+                        {
+                            item.IsProcessed = true;
+                            await _actionService.SaveCarbonOffsettingAction(item);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error while executing Job" + ex);
+                    }
                 }
             }
-
-            var offsettingActions = await _actionService.GetOffsettingActionsToBeProcessed();
-            foreach (var item in offsettingActions)
+            catch (Exception ex)
             {
-                var walletId = item.SupplyChainPartner.WalletId;
-                var account = new Account(walletId);
-                var web3 = new Web3(account, _blockchainURL);
-                var transaction = await web3.Eth.GetEtherTransferService()
-                .TransferEtherAndWaitForReceiptAsync(walletId, (decimal)item.Offset);
-                var transactionId = transaction.TransactionHash;
-                item.EmissionTransactionId = transactionId;
-
-                var updateResult = await _supplyChainPartnerService.UpdateScpCredits(item.SupplyChainPartnerId, item.Offset);
-
-                if (!updateResult)
-                {
-                    item.IsProcessed = true;
-                    await _actionService.SaveCarbonOffsettingAction(item);
-                }
+                Console.WriteLine("Error while executing Job:" + ex);
             }
         }
     }
