224eee628319c460a2e26c787e75f33ac7b54a11
NourCheriff <nourcherif.pitos25@gmail.com>
2025-03-31 22:23:39 +0200

    add: testing accounts to genesis.json and set carbon credits to those accounts

--- frontend/contracts/CarbonCredits.sol
+++ frontend/contracts/CarbonCredits.sol
@@ -6,5 +6,8 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract CarbonCredits is ERC20 {
     constructor() ERC20("CarbonCredits", "CC") {
         _mint(msg.sender, 0);
+        _mint(address(0xFE3B557E8Fb62b89F4916B721be55cEb828dBd73), 100);
+        _mint(address(0x627306090abaB3A6e1400e9345bC60c78a8BEf57), 40);
+        _mint(address(0xf17f52151EbEF6C7334FAD080c5704D77216b732), 20);
     }
 }
